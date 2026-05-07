c918081433c4087ab3eddeab943b5135361028cb
SANMaxworld <saneditsop@gmail.com>
2026-05-07 13:07:38 +0530

    Create search-data.js

---
+++ search-data.js
@@ -0,0 +1,32 @@
+// Max World Hub - Global Search Database
+// Is file mein Page 2, Page 3 aur aage ke saare anime ka data aayega.
+
+const extraAnimeList = [
+    {
+        title: "Naruto Shippuden",
+        img: "assets/naruto_poster.jpeg", // Tumhare assets folder mein jo image ho uska path
+        link: "page2.html#action",
+        desc: "Action • All Episodes"
+    },
+    {
+        title: "Demon Slayer Season 4",
+        img: "assets/ds4_poster.jpeg",
+        link: "page2.html#trending",
+        desc: "Trending • New Season"
+    },
+    {
+        title: "Solo Leveling S2",
+        img: "assets/solo_poster.jpeg",
+        link: "page3.html#action",
+        desc: "Action • Coming Soon"
+    },
+    {
+        title: "Death Note",
+        img: "assets/death_note.jpeg",
+        link: "page2.html#horror",
+        desc: "Thriller • Full Series"
+    }
+];
+
+// Tip: Naya anime add karne ke liye upar waale block ko copy karein 
+// aur niche ek comma (,) laga kar paste kar dein.
