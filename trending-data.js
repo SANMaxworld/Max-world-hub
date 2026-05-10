014f85964166bd5a61ce437f4d641f67781a56a6
SANMaxworld <saneditsop@gmail.com>
2026-05-10 18:23:44 +0530

    Delete trending-data.js

--- trending-data.js
+++
@@ -1,5 +0,0 @@
-const trendingData = [
-  { "title": "Attack on Titan", "img": "assets/A.O.T bannar.jpeg", "desc": "Action, Drama", "link": "#", "lang": "HINDI" },
-  { "title": "Solo Leveling", "img": "assets/S.L Bannar.jpeg", "desc": "Fantasy, Action", "link": "#", "lang": "HINDI" },
-  { "title": "Demon Slayer", "img": "assets/D.S movie.jpeg", "desc": "Action, Adventure", "link": "#", "lang": "HINDI" }
-];
