1e2560ce7a08ce2d92339de369c1d857281e206e
SANMaxworld <saneditsop@gmail.com>
2026-05-22 20:44:06 +0530

    Update data.js

--- data.js
+++ data.js
@@ -73,11 +73,11 @@ const animeData = [
         categories: ["series"]
     },
     {
-        id: "one-piece",
-        title: "One Piece",
+        id: "chainsaw-Man",
+        title: "chainshow Man",
         img: "assets/adv_sample.jpeg",
         lang: "HINDI",
-        link: "#",
+        link: "C.Man S1",
         desc: "Adventure",
         categories: ["adventure"]
     },
